e3ef63b2c4c876c0b01b62cb9d4ef57e7285d8f7
EC2 Default User <ec2-user@ip-172-31-12-100.ec2.internal>
2022-07-19 16:00:02 +0000

    20220719

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,6 +13,7 @@ class Public::CustomersController < ApplicationController
   def update
     @customer=current_customer
     if @customer.update(customer_params)
+    flash[:notice] = "登録情報を更新しました。"
     redirect_to customers_my_page_path
     else
     render:edit
